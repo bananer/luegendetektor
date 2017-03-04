f06ee74aa13a25f25559ef74d714d9a92ec7dd8a
Philip <ich@philipfrank.de>
2017-03-04 15:05:00 +0100

    fix things

--- serve.py
+++ serve.py
@@ -1,7 +1,7 @@
 from sklearn.externals import joblib
 import web
 from web import form
-
+import os
 
 myform = form.Form(
     form.Textarea("text")
@@ -10,18 +10,18 @@ myform = form.Form(
 render = web.template.render('templates/')
 
 urls = (
-    '/(.*)', 'hello'
-	'/images/(.*)', 'images' #this is where the image folder is located....
+    '/', 'hello',
+    '/images/(.*)', 'images',
 )
 app = web.application(urls, globals())
 
 
 class hello:
-    def GET(self, name):
+    def GET(self):
         form = myform()
         return render.index(form, "", "")
 
-    def POST(self, name):
+    def POST(self):
         form = myform()
         text = ""
         result = ""
@@ -32,19 +32,20 @@ class hello:
             result = predicted[0]
 
         return render.index(form, text, result)
-		
-import os
+
+
 class images:
-    def GET(self,name):
-        ext = name.split(".")[-1] # Gather extension
+    def GET(self, name):
+        ext = name.split(".")[-1]  # Gather extension
 
         cType = {
-            "png":"images/png",
-            "jpg":"images/jpeg"          }
+            "png": "images/png",
+            "jpg": "images/jpeg"
+        }
 
         if name in os.listdir('images'):  # Security
-            web.header("Content-Type", cType[ext]) # Set the Header
-            return open('images/%s'%name,"rb").read() # Notice 'rb' for reading images
+            web.header("Content-Type", cType[ext])  # Set the Header
+            return open('images/%s' % name, "rb").read()  # Notice 'rb' for reading images
         else:
             raise web.notfound()
 
